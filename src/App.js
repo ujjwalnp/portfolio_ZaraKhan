80c7596bf71795b4dd0b9b9f6ef164b8c808f460
Zara Khan <ZaraMustafa@ymail.com>
2022-02-10 11:50:56 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import HeaderThree from "./components/header";
 import Experties from "./components/experties";
+import { skills } from "./data";
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
 
       <About />
       <Experties />
+      <Skills />
       <Experience />
       <Projects />
       <Contact />
